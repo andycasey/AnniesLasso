18687b7fb693377a3a2653fc6a55b02ecb248d0a
Andy Casey <andycasey@gmail.com>
2015-11-18 12:51:47 +0000

    Fix Travis build

---
+++ code/tests/test_ci.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+An assertion test to ensure continuous integration is running properly.
+"""
+
+def test_continuous_integration():
+    return True
