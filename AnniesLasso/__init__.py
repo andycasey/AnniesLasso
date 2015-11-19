24158307dc58a4d724418c5fad65f923d485825c
Andy Casey <andycasey@gmail.com>
2015-11-19 13:23:37 +0000

    Shut up and speak up [ci skip]

--- AnniesLasso/__init__.py
+++ AnniesLasso/__init__.py
@@ -6,5 +6,17 @@ from __future__ import (division, print_function, absolute_import,
 
 __version__ = "0.1.0"
 
+import logging
+from numpy import RankWarning
+from warnings import simplefilter
+
+from .cannon import *
 from .model import *
 from . import utils
+
+
+logger = logging.getLogger("cannon")
+logger.setLevel(logging.CRITICAL)
+
+simplefilter("ignore", RankWarning)
+simplefilter("ignore", RuntimeWarning)
