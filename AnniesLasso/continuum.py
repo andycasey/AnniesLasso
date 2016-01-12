7270936ad2c8d5dc7306acf3b1f27178ffffff99
Andy Casey <andycasey@gmail.com>
2016-01-12 23:04:06 +0000

    Update continuum normalization (now separated from the APOGEE-specific script)

--- AnniesLasso/continuum.py
+++ AnniesLasso/continuum.py
@@ -2,73 +2,157 @@
 # -*- coding: utf-8 -*-
 
 """
-Functionality for continuum normalization.
+Continuum-normalization.
 """
 
 from __future__ import (division, print_function, absolute_import,
                         unicode_literals)
 
-__all__ = ["chebyshev"]
+__all__ = ["fit", "fit_sines_and_cosines"]
 
 import numpy as np
 
 
-def chebyshev(dispersion, fluxes, flux_uncertainties, continuum_mask,
-    degree=2, regions=None):
+def _continuum_design_matrix(dispersion, L, order):
     """
-    Fit Chebyshev polynomials to the flux values in the continuum mask. Fluxes 
-    from multiple stars can be given, and the resulting continuum will have the
-    same shape as `fluxes`.
+    Build a design matrix for the continuum determination, using sines and
+    cosines.
 
     :param dispersion:
-        The dispersion values for each of the flux values.
+        An array of dispersion points.
 
-    :param fluxes:
-        The flux values.
+    :param L:
+        The length-scale for the sine and cosine functions.
 
-    :param flux_uncertainties:
-        The observational uncertainties associated with the fluxes.
-
-    :param continuum_mask:
-        A mask for continuum pixels to use.
-
-    :param degree: [optional]
-        The degree of Chebyshev polynomial to use in each region.
-
-    :param regions: [optional]
-        Split up the continuum fitting into different wavelength regions. For
-        example, APOGEE spectra could be splitted into three chunks:
-        `[(15150, 15800), (15890, 16430), (16490, 16950)]`
+    :param order:
+        The number of sines and cosines to use in the fit.
     """
 
+    L, dispersion = float(L), np.array(dispersion)
+    scale = 2 * (np.pi / L)
+    return np.vstack([
+        np.ones_like(dispersion).reshape((1, -1)), 
+        np.array([
+            [np.cos(o * scale * dispersion), np.sin(o * scale * dispersion)] \
+            for o in range(1, order + 1)]).reshape((2 * order, dispersion.size))
+        ])
+
+
+def fit_sines_and_cosines(dispersion, flux, ivar, continuum_pixels,
+    L=1400, order=3, regions=None, fill_value=1.0, full_output=False, **kwargs):
+    """
+    Fit the flux values of pre-defined continuum pixels using a sum of sine and
+    cosine functions.
+
+    :param dispersion:
+        The dispersion values.
+
+    :param flux:
+        The flux values for all pixels, as they correspond to the `dispersion`
+        array.
+
+    :param ivar:
+        The inverse variances for all pixels, as they correspond to the
+        `dispersion` array.
+
+    :param continuum_pixels:
+        A mask that selects pixels that should be considered as 'continuum'.
+
+    :param L: [optional]
+        The length scale for the sines and cosines.
+
+    :param order: [optional]
+        The number of sine/cosine functions to use in the fit.
+
+    :param regions: [optional]
+        Specify sections of the spectra that should be fitted separately in each
+        star. This may be due to gaps between CCDs, or some other physically-
+        motivated reason. These values should be specified in the same units as
+        the `dispersion`, and should be given as a list of `[(start, end), ...]`
+        values. For example, APOGEE spectra have gaps near the following
+        wavelengths which could be used as `regions`:
+
+        >> regions = ([15090, 15822], [15823, 16451], [16452, 16971])
+
+    :param fill_value: [optional]
+        The continuum value to use for when no continuum was calculated for that
+        particular pixel (e.g., the pixel is outside of the `regions`).
+
+    :param full_output: [optional]
+        If set as True, then a metadata dictionary will also be returned.
+
+    :returns:
+        The continuum values for all pixels, and optionally a dictionary that
+        contains metadata for the fit.
+    """
+
+    scalar = kwargs.pop("__magic_scalar", 1e-6) # MAGIC
+    flux, ivar = np.atleast_2d(flux), np.atleast_2d(ivar)
+
     if regions is None:
-        region_masks = [np.ones_like(dispersion, dtype=bool)]
-    else:
-        region_masks = \
-            [(e >= dispersion) * (dispersion >= s) for s, e in regions]
+        regions = [(dispersion[0], dispersion[-1])]
 
-    dispersion = np.array(dispersion).flatten()
-    fluxes = np.atleast_2d(fluxes)
-    flux_uncertainties = np.atleast_2d(flux_uncertainties)
+    region_masks = []
+    region_matrices = []
+    continuum_masks = []
+    continuum_matrices = []
+    for start, end in regions:
 
-    N_stars = fluxes.shape[0]
-    assert fluxes.shape[1] == dispersion.size
+        # Build the masks for this region.
+        si, ei = np.searchsorted(dispersion, (start, end))
+        region_masks.append(
+            (end >= dispersion) * (dispersion >= start))
+        continuum_masks.append(continuum_pixels[
+            (ei >= continuum_pixels) * (continuum_pixels >= si)])
 
-    i_variances = 1.0/flux_uncertainties**2
+        # Build the design matrices for this region.
+        region_matrices.append(
+            _continuum_design_matrix(dispersion[region_masks[-1]], L, order))
+        continuum_matrices.append(
+            _continuum_design_matrix(dispersion[continuum_masks[-1]], L, order))
 
-    # Use only continuum pixels.
-    i_variances[:, ~continuum_mask] += 200**2
-    #i_variances[~np.isfinite(i_variances)] = 0
-    
-    continuum = np.ones_like(fluxes, dtype=float)
-    for i, (flux, i_var) in enumerate(zip(fluxes, i_variances)):
-        for region_mask in region_masks:
-            fitted_mask = region_mask * continuum_mask
-            f = np.polynomial.chebyshev.Chebyshev.fit(
-                x=dispersion[fitted_mask], y=flux[fitted_mask],
-                w=i_var[fitted_mask], deg=degree)
+        # TODO: ISSUE: Check for overlapping regions and raise an warning.
 
-            continuum[i, region_mask] *= f(dispersion[region_mask])
+    metadata = []
+    continuum = np.ones_like(flux) * fill_value
+    for i in range(flux.shape[0]):
 
-    return continuum
+        # Get the flux and inverse variance for this object.
+        object_metadata = []
+        object_flux, object_ivar = (flux[i], ivar[i])
 
+        # Normalize each region.
+        for region_mask, region_matrix, continuum_mask, continuum_matrix in \
+        zip(region_masks, region_matrices, continuum_masks, continuum_matrices):
+            if continuum_mask.size == 0:
+                # Skipping..
+                object_metadata.append([order, L, fill_value, scalar, [], None])
+                continue
+
+            # We will fit to continuum pixels only.   
+            continuum_disp = dispersion[continuum_mask] 
+            continuum_flux, continuum_ivar \
+                = (object_flux[continuum_mask], object_ivar[continuum_mask])
+
+            # Solve for the amplitudes.
+            M = continuum_matrix
+            MTM = np.dot(M, continuum_ivar[:, None] * M.T)
+            MTy = np.dot(M, (continuum_ivar * continuum_flux).T)
+
+            eigenvalues = np.linalg.eigvalsh(MTM)
+            MTM[np.diag_indices(len(MTM))] += scalar * np.max(eigenvalues)
+            eigenvalues = np.linalg.eigvalsh(MTM)
+            condition_number = max(eigenvalues)/min(eigenvalues)
+
+            amplitudes = np.linalg.solve(MTM, MTy)
+            continuum[i, region_mask] = np.dot(region_matrix.T, amplitudes)
+            object_metadata.append(
+                (order, L, fill_value, scalar, amplitudes, condition_number))
+
+        metadata.append(object_metadata)
+
+    return (continuum, metadata) if full_output else continuum
+
+
+# Because this is best.
+fit = fit_sines_and_cosines
