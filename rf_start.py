56e2636448c9c3fc9cd139a447b91c8132c3ca16
Andy Casey <andycasey@gmail.com>
2016-02-14 15:12:21 +0000

    Updated script

--- rf_start.py
+++ rf_start.py
@@ -8,7 +8,7 @@ import AnniesLasso as tc
 
 
 
-a = tc.load_model("gridsearch-2.0-3.0.model", threads=4)
+a = tc.load_model("gridsearch-2.0-3.0-s2hh.model", threads=4)
 
 # Load the data.
 PATH, CATALOG, FILE_FORMAT = ("/Users/arc/research/apogee/", "apogee-rg.fits",
@@ -25,6 +25,7 @@ normalized_ivar = np.memmap(
     mode="c", dtype=float).reshape(normalized_flux.shape)
 
 # Split up the data into ten random subsets.
+np.random.seed(123) # For reproducibility.
 q = np.random.randint(0, 10, len(labelled_set)) % 10
 
 validate_set = (q == 0)
@@ -36,6 +37,11 @@ a._labelled_set = labelled_set[train_set]
 a._normalized_flux = normalized_flux[train_set]
 a._normalized_ivar = normalized_ivar[train_set]
 
+#a._set_s2_by_hogg_heuristic()
+#a.train()
+#a.save("gridsearch-2.0-3.0-s2hh.model", overwrite=True)
+
+raise a
 
 b = tc.L1RegularizedCannonModel(labelled_set[validate_set], 
     normalized_flux[validate_set], normalized_ivar[validate_set], dispersion,
