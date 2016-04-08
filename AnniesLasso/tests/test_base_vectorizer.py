010a457ef8fecdbd38244e8266215ed35048779c
Andy Casey <andycasey@gmail.com>
2016-04-08 10:25:31 +0100

    Fixed tests for base vectorizer

--- AnniesLasso/tests/test_base_vectorizer.py
+++ AnniesLasso/tests/test_base_vectorizer.py
@@ -5,90 +5,88 @@
 Unit tests for the base vectorizer.
 """
 
+import numpy as np
 import os
 import tempfile
 import unittest
 from AnniesLasso.vectorizer import base
 
 
-
-
-
 class TestBaseVectorizerInitialization(unittest.TestCase):
 
-    labels = ("a", "b", "c", "d", "e")
+    label_names = ("a", "b", "c", "d", "e")
 
     def test_incompatible_scale_length(self):
         with self.assertRaises(ValueError):
-            base.BaseVectorizer(self.labels,
-                np.random.uniform(size=len(self.labels)),
-                np.random.uniform(size=len(self.labels) + 1),
+            base.BaseVectorizer(self.label_names,
+                np.random.uniform(size=len(self.label_names)),
+                np.random.uniform(size=len(self.label_names) + 1),
                 [])
 
     def test_incompatible_fiducial_length_1(self):
         with self.assertRaises(ValueError):
-            base.BaseVectorizer(self.labels,
-                np.random.uniform(size=len(self.labels) - 1),
-                np.random.uniform(size=len(self.labels)),
+            base.BaseVectorizer(self.label_names,
+                np.random.uniform(size=len(self.label_names) - 1),
+                np.random.uniform(size=len(self.label_names)),
                 [])
 
 
     def test_non_finite_fiducials(self):
         with self.assertRaises(ValueError):
-            base.BaseVectorizer(self.labels,
-                ([0] * len(self.labels)) + [np.nan],
-                np.random.uniform(size=len(self.labels)),
+            base.BaseVectorizer(self.label_names,
+                ([0] * len(self.label_names)) + [np.nan],
+                np.random.uniform(size=len(self.label_names)),
                 [])
 
 
     def test_infinite_fiducials(self):
         with self.assertRaises(ValueError):
-            base.BaseVectorizer(self.labels,
-                ([0] * len(self.labels)) + [+np.inf],
-                np.random.uniform(size=len(self.labels)),
+            base.BaseVectorizer(self.label_names,
+                ([0] * len(self.label_names)) + [+np.inf],
+                np.random.uniform(size=len(self.label_names)),
                 [])
 
         with self.assertRaises(ValueError):
-            base.BaseVectorizer(self.labels,
-                ([0] * len(self.labels)) + [-np.inf],
-                np.random.uniform(size=len(self.labels)),
+            base.BaseVectorizer(self.label_names,
+                ([0] * len(self.label_names)) + [-np.inf],
+                np.random.uniform(size=len(self.label_names)),
                 [])
 
 
     def test_non_finite_scales(self):
         with self.assertRaises(ValueError):
-            base.BaseVectorizer(self.labels,
-                np.random.uniform(size=len(self.labels)),
-                ([0] * len(self.labels)) + [np.nan],
+            base.BaseVectorizer(self.label_names,
+                np.random.uniform(size=len(self.label_names)),
+                ([0] * len(self.label_names)) + [np.nan],
                 [])
 
 
     def test_infinite_scales(self):
         with self.assertRaises(ValueError):
-            base.BaseVectorizer(self.labels,
-                np.random.uniform(size=len(self.labels)),
-                ([0] * len(self.labels)) + [+np.inf],
+            base.BaseVectorizer(self.label_names,
+                np.random.uniform(size=len(self.label_names)),
+                ([0] * len(self.label_names)) + [+np.inf],
                 [])
 
         with self.assertRaises(ValueError):
-            base.BaseVectorizer(self.labels,
-                np.random.uniform(size=len(self.labels)),
-                ([0] * len(self.labels)) + [-np.inf],
+            base.BaseVectorizer(self.label_names,
+                np.random.uniform(size=len(self.label_names)),
+                ([0] * len(self.label_names)) + [-np.inf],
                 [])
 
 
     def test_negative_scales(self):
         with self.assertRaises(ValueError):
-            base.BaseVectorizer(self.labels,
-                np.random.uniform(size=len(self.labels)),
-                np.random.uniform([-2, -1], size=len(self.labels)),
+            base.BaseVectorizer(self.label_names,
+                np.random.uniform(size=len(self.label_names)),
+                np.random.uniform([-2, -1], size=len(self.label_names)),
                 [])
 
 
     def test_serialization(self):
-        before = base.BaseVectorizer(self.labels,
-            np.random.uniform(size=len(self.labels)),
-            np.random.uniform(size=len(self.labels)),
+        before = base.BaseVectorizer(self.label_names,
+            np.random.uniform(size=len(self.label_names)),
+            np.random.uniform(size=len(self.label_names)),
             [(1, 0), (2, 1.0)]) # ignored, just for testing.
 
         _, path = tempfile.mkstemp()
@@ -108,3 +106,39 @@ class TestBaseVectorizerInitialization(unittest.TestCase):
 
         if os.path.exists(path):
             os.remove(path)
+
+
+    def test_transforms(self):
+
+        v = base.BaseVectorizer(self.label_names,
+            np.random.uniform(size=len(self.label_names)),
+            np.random.uniform(size=len(self.label_names)),
+            [])
+
+        for labels in np.random.uniform(size=(100, len(self.label_names))):
+            self.assertEqual(labels, v._inv_transform(v._transform(labels)))
+
+
+    def test_no_monkey_patching(self):
+
+        v = base.BaseVectorizer(self.label_names,
+            np.random.uniform(size=len(self.label_names)),
+            np.random.uniform(size=len(self.label_names)),
+            [])
+
+        for method in (v, v.get_label_vector, v.get_label_vector_derivative,
+            v.get_approximate_labels, v.get_human_readable_label_vector):
+
+            with self.assertRaises(NotImplementedError):
+                method(None)
+
+
+    def test_repr(self):
+        v = base.BaseVectorizer(self.label_names,
+            np.random.uniform(size=len(self.label_names)),
+            np.random.uniform(size=len(self.label_names)),
+            [])
+        _ = "{}".format(v.__str__())
+        __ = "{}".format(v.__repr__())
+
+
