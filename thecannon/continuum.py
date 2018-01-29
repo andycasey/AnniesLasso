bf1eb1b0936434713f32fedb4545a331968b31f1
Andy Casey <andycasey@gmail.com>
2018-01-29 12:48:14 +1100

    add normalize func

--- thecannon/continuum.py
+++ thecannon/continuum.py
@@ -8,7 +8,7 @@ Continuum-normalization.
 from __future__ import (division, print_function, absolute_import,
                         unicode_literals)
 
-__all__ = ["sines_and_cosines"]
+__all__ = ["normalize", "sines_and_cosines"]
 
 import logging
 import numpy as np
@@ -178,3 +178,60 @@ def sines_and_cosines(dispersion, flux, ivar, continuum_pixels, L=1400, order=3,
 
     return (continuum, metadata) 
     
+
+def normalize(dispersion, flux, ivar, continuum_pixels, L=1400, order=3, 
+    regions=None, fill_value=1.0, **kwargs):
+    """
+    Pseudo-continuum-normalize the flux using a defined set of continuum pixels
+    and a sum of sine and cosine functions.
+
+    :param dispersion:
+        The dispersion values.
+
+    :param flux:
+        The flux values for all pixels, as they correspond to the `dispersion`
+        array.
+
+    :param ivar:
+        The inverse variances for all pixels, as they correspond to the
+        `dispersion` array.
+
+    :param continuum_pixels:
+        A mask that selects pixels that should be considered as 'continuum'.
+
+    :param L: [optional]
+        The length scale for the sines and cosines.
+
+    :param order: [optional]
+        The number of sine/cosine functions to use in the fit.
+
+    :param regions: [optional]
+        Specify sections of the spectra that should be fitted separately in each
+        star. This may be due to gaps between CCDs, or some other physically-
+        motivated reason. These values should be specified in the same units as
+        the `dispersion`, and should be given as a list of `[(start, end), ...]`
+        values. For example, APOGEE spectra have gaps near the following
+        wavelengths which could be used as `regions`:
+
+        >> regions = ([15090, 15822], [15823, 16451], [16452, 16971])
+
+    :param fill_value: [optional]
+        The continuum value to use for when no continuum was calculated for that
+        particular pixel (e.g., the pixel is outside of the `regions`).
+
+    :param full_output: [optional]
+        If set as True, then a metadata dictionary will also be returned.
+
+    :returns:
+        The continuum values for all pixels, and a dictionary that contains 
+        metadata about the fit.
+    """
+    continuum, metadata = sines_and_cosines(dispersion, flux, ivar, **kwargs)
+
+    normalized_flux = flux/continuum
+    normalized_ivar = continuum * ivar * continuum
+    normalized_flux[normalized_ivar == 0] = 1.0
+    
+    return (normalized_flux, normalized_ivar, continuum, metadata)
+
+
